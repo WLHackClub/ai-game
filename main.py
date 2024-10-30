fa942e827c555710be9f641a06f3818ce2cf60ff
greatericontop <greateric@protonmail.com>
2024-10-29 22:33:10 -0400

    formatting

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import torch
 hf_token = ''
 model_name = 'meta-llama/Meta-Llama-3-8B-Instruct'
 
-bnb_config = transformers.BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16,
-)
+bnb_config = transformers.BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16)
 model = transformers.AutoModelForCausalLM.from_pretrained(model_name, device_map='auto', quantization_config=bnb_config, token=hf_token)
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_name, token=hf_token)
 generator = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, pad_token_id=tokenizer.eos_token_id)
